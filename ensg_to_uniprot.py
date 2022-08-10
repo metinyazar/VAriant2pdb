948aa2dce8ab56a6eeba7c02195925cea4926c4f
metinyazar <32467807+metinyazar@users.noreply.github.com>
2022-08-10 14:24:21 +0300

    ...

--- ensg_to_uniprot.py
+++ ensg_to_uniprot.py
@@ -5,8 +5,8 @@ import pandas as pd
 def ensg_uniprot(ensg_geneid, ensemble_sift):
     ''' # This function is to find the UniProt IDs of each variant from the Gene IDs (ENSG encoded) from the
     Ensembl VEP system in the SIFTS file. (Can be downloaded from http://ftp.ebi.ac.uk/pub/databases/msd/sifts/flatfiles/csv/pdb_chain_ensembl.csv.gz.)'''
-    sifts_ensemble = pd.read_csv(ensemble_sift,header=1)
     vep_output = pd.read_excel(ensg_geneid)
+    sifts_ensemble = pd.read_csv(ensemble_sift,header=1)
     data = []
     index = vep_output.index.values
     for i in index:
